3b945954d7523da86d11fd958e486ddba29155b9
Davi Cabral <davicabral94@gmail.com>
2016-04-30 11:37:49 -0300

    Inclusao do angular, requests, feedback do login

--- js/angular-controllers/angular-controllers.js
+++ js/angular-controllers/angular-controllers.js
@@ -2,11 +2,41 @@
  * Created by Davi on 29/04/16.
  */
 
-var app = angular.module("postit-login", []);
+var app = angular.module("postit-login", [
+    'angular-ladda'
+]);
 
 app.controller( "loginController" , function ($scope, $http) {
 
+    //Variável responsável para dar feedback sobre problemas no processo de login
+    $scope.loginErro = false;
+    $scope.loginLoading = false;
+
     $scope.onSubmit = function () {
-        console.log("Formulário completo");
+        $scope.loginLoading = true;
+        var params = {
+            login: $scope.form.login,
+            password: $scope.form.password
+        };
+
+        $http.post('http://localhost:3000/user/login', params).then(onLoginSuccess, onLoginFail)
+
+        //Função chamada quando a requisição é processada normalmente
+        function onLoginSuccess(response) {
+            $scope.loginLoading = false;
+            $scope.loginErro = !response.data.hasUser;
+            if(response.data.hasUser) {
+                //Fazer processo de login e armazenar usuário para passar para a outra tela
+                console.log(response);
+            }
+
+        }
+
+        //Função chamada quando há algum erro no processamento da função. Ex: Timeout ou servidor não acessível.
+        function onLoginFail(err) {
+            $scope.loginLoading = false;
+            console.log("Erro na requisição");
+            console.log(err);
+        }
     }
 });
