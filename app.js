75dae5e34d50e7bc656e542048c84df63ed943d0
Davi Cabral <davicabral94@gmail.com>
2016-05-01 18:15:37 -0300

    bug fix para deploy no heroku. Conversao para templates em jade

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
  * Created by Davi on 01/05/16.
  */
 var express = require('express');
+var path = require('path');
+
 var app = express();
 
 var index = require('./routes/index');
@@ -9,12 +11,16 @@ var user = require('./routes/user');
 var postit = require('./routes/postit');
 
 
-app.set('port', (process.env.PORT || 3000));
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', (process.env.PORT || 5500));
 
 app.use(express.static(__dirname + '/public'));
 app.use('/', index);
-app.use('/user', user);
-app.use('/postit', postit);
+//app.use('/user', user);
+//app.use('/postit', postit);
 
 
 app.listen(app.get('port'), function() {
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@
  * Created by Davi on 01/05/16.
  */
 var express = require('express');
+var path    = require("path");
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile('../view/index.html');
+    res.render('index');
 });
 
 module.exports = router;
