e696c7db5d36ab0978e1262c72fbdfb692e0203e
Davi Cabral <davicabral94@gmail.com>
2016-05-03 01:04:49 -0300

    Ajuste no posicionamento dos botoes dos postits

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use('/postit', postit);
 db.sequelize.sync({force: true}).then(function () {
 
     db.User.create({
-        login: 'carlos',
+        login: 'herakles',
         password: '7c4a8d09ca3762af61e59520943dc26494f8941b'
     });
 
